a8359c33fffab70d17a520dfb0041141cc97b9f8
nuch0208 <nutchanet2887@gmail.com>
2024-10-04 13:03:16 +0700

    update

--- obj/Debug/net8.0/AuthenCard.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthenCard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenCard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d41892a596ff3876d51e8f0528ff60ba5ec2d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a16a3e465e110bb6556715bfad8cf242da66e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenCard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenCard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
